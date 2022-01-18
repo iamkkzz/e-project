c0c8aa797825b2783722ca7ec6ad88c9a9245f12
kkzz <2476479069@qq.com>
2022-01-18 22:04:19 +0800

    项目初始化

---
+++ mall-ware/src/test/java/com/kkzz/mall/ware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkzz.mall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
