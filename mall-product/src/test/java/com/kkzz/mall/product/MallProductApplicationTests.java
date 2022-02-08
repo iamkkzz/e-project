43a2fb7a1c74c439657e10be73cb1cb4e624657f
kkzz <2476479069@qq.com>
2022-02-08 17:37:43 +0800

    完善1

--- mall-product/src/test/java/com/kkzz/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/kkzz/mall/product/MallProductApplicationTests.java
@@ -3,7 +3,9 @@ package com.kkzz.mall.product;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.kkzz.mall.product.entity.BrandEntity;
+import com.kkzz.mall.product.service.AttrGroupService;
 import com.kkzz.mall.product.service.BrandService;
+import com.kkzz.mall.product.service.CategoryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,18 +21,14 @@ class MallProductApplicationTests {
     @Autowired
     BrandService brandService;
 
+    @Autowired
+    CategoryService categoryService;
     @Test
     void contextLoads() {
-//        BrandEntity brandEntity = new BrandEntity();
-//        brandEntity.setBrandId(1L);
-//        brandEntity.setDescript("无敌华为");
-////        brandService.save(brandEntity);
-////        System.out.println("保存成功!");
-//        brandService.updateById(brandEntity);
-//        System.out.println("修改成功!");
-
-        List<BrandEntity> brand_id = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
-        brand_id.forEach(System.out::println);
+        Long[] catlogPath = categoryService.findCatlogPath(223L);
+        for (Long aLong : catlogPath) {
+            System.out.println(aLong);
+        }
     }
 
 }
